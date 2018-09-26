054571c8c66e84127185415d413e514f21e596f1
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-09-26 13:19:36 -0400

    added functions for entering and exiting proj, adding swap func for clas

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
-const projectImages = document.querySelectorAll('.project');
-function displayCardStyles(){
+const projectTile = document.querySelectorAll('.project');
+const projImages = document.querySelectorAll('.project-image');
+const projDesc = document.querySelector('.project-description');
+projectTile.forEach(project => {
+  project.addEventListener('mouseenter', (event) => {
+    showProjDescription(event.target.id);
+  })
+  project.addEventListener('mouseleave', (event) => {
+    hideProjectDescription(event.target.id);
+  })
+})
+function showProjDescription(proj) {
+  switch (proj) {
+    case "1":
+      swapHidden(0);
+      break;
+    case "2":
+      console.log(proj);
+      break;
+    case "3":
+      console.log(proj);
+      break;
+  };
+}
+function hideProjectDescription(proj) {
+  switch (proj) {
+    case "1":
+      console.log(proj);
+      break;
+    case "2":
+      console.log(proj);
+      break;
+    case "3":
+      console.log(proj);
+      break;
+  };
+}
+function swapHidden(currentClass){
+  console.log(currentClass);
   
 }

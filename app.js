92626002de75fb0c4a9e457c4586cc453cfe11fa
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2019-02-08 12:23:21 -0700

    added eslint, formated app.js

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const projDesc = document.querySelectorAll('.project-description');
 projectTile.forEach(project => {
   project.addEventListener('mouseenter', (event) => {
     showProjDescription(event.target.id);
-  })
+  });
   project.addEventListener('mouseleave', (event) => {
     hideProjectDescription(event.target.id);
-  })
-})
+  });
+});
 function showProjDescription(projNum) {  
-  projImages[projNum].classList.add('hidden')
-  projDesc[projNum].classList.remove('hidden')  
+  projImages[projNum].classList.add('hidden');
+  projDesc[projNum].classList.remove('hidden');  
 }
 function hideProjectDescription(projNum) {
-  projImages[projNum].classList.remove('hidden')
-  projDesc[projNum].classList.add('hidden') 
+  projImages[projNum].classList.remove('hidden');
+  projDesc[projNum].classList.add('hidden'); 
 }

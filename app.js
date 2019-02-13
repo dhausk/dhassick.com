582e9952dbe53ddfcec04ed3d4f2ea27aa5db129
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2019-02-12 19:56:41 -0700

    showing proj desc for mobile, added hover for a tags

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ const projectTile = document.querySelectorAll('.project-card');
 const projImages = document.querySelectorAll('.project-image');
 const projDesc = document.querySelectorAll('.project-description');
 
-projectTile.forEach(project => {
-  project.addEventListener('mouseenter', (event) => {
-    showProjDescription(event.target.id );
-  });
-  project.addEventListener('mouseleave', (event) => {
-    hideProjectDescription(event.target.id );
-  });
-});
+function showFullCard() {
+  projDesc.forEach(proj => proj.classList.remove('hidden'));
+}
 function showProjDescription(projNum) {  
   projImages[projNum].classList.add('hidden');
   projDesc[projNum].classList.remove('hidden');  
@@ -18,3 +13,15 @@ function hideProjectDescription(projNum) {
   projImages[projNum].classList.remove('hidden');
   projDesc[projNum].classList.add('hidden'); 
 }
+projectTile.forEach(project => {
+  if (window.matchMedia('(max-width: 700px)').matches) {
+    showFullCard();
+  } else {
+    project.addEventListener('mouseenter', (event) => {
+      showProjDescription(event.target.id);
+    });
+    project.addEventListener('mouseleave', (event) => {
+      hideProjectDescription(event.target.id);
+    });
+  }
+});
